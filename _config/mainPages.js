29676e2c8c92c404d70b42c2b2ce2b1b2058448a
Angel Barrios <ae.barrios@hotmail.com>
2022-04-12 14:46:12 -0400

    +update: enable mySubscription page in qplan

--- _config/mainPages.js
+++ _config/mainPages.js
@@ -1,12 +1,12 @@
 export default {
   userSubscriptions: {
-    permission: 'iplan.plans.manage',
+    permission: 'iplan.plans.ownPlans',
     activated: true,
     path: '/plans/me',
     name: 'qplan.admin.my.plans',
     page: () => import('@imagina/qplan/_pages/main/myPlan'),
     layout: () => import('@imagina/qsite/_layouts/master.vue'),
-    title: 'iplan.cms.sidebar.adminUserSubscriptions',
+    title: 'iplan.cms.sidebar.adminSubscriptions',
     icon: 'fas fa-window-restore',
     authenticated: true,
     subHeader: {
