f3b5d5da883c3664ad52571c2156f45163204c60
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-06-25 16:07:39 -0500

    + add translations

--- _i18n/es/index.js
+++ _i18n/es/index.js
@@ -1,7 +1,71 @@
-import layout from '../../_i18n/es/layout'
-import sidebar from '../../_i18n/es/sidebar'
-
 export default {
-  layout,
-  sidebar
+ "iplan": {
+    "cms": {
+      "sidebar": {
+        "adminGroup": "Planes",
+        "adminCategories": "Categorías",
+        "adminUserSubscriptions": "Planes disponibles",
+        "createCategories": "Crear Categoría",
+        "editCategories": "Editar Categoría",
+        "adminPlans": "Planes",
+        "createPlans": "Crear Plan",
+        "editPlans": "Editar Plan",
+        "adminEntityPlans": "Entidades del Plan",
+        "createEntityPlans": "Crear Entidad del Plan",
+        "editEntityPlans": "Editar Entidad del Plan",
+        "adminLimits": "Límites del Plan",
+        "createLimits": "Crear Límite del Plan",
+        "editLimits": "Editar Límite del Plan",
+        "adminSubscriptions": "Suscripciones",
+        "createSubscriptions": "Crear Suscripción",
+        "editSubscriptions": "Editar Suscripción",
+        "adminSubscriptionLimits": "Límites de Suscripción",
+        "createSubscriptionLimits": "Crear Límite de Suscripción",
+        "editSubscriptionLimits": "Editar Límite de Suscripción",
+        "panelSubscriptions": "Mis Suscripciones",
+        "adminMySubscriptions": "Mis Suscripciones"
+      },
+      "newCategory": "Nueva Categoría",
+      "updateCategory": "Actualizar Categoría",
+      "newPlan": "Nuevo Plan",
+      "updatePlan": "Actualizar Plan",
+      "newEntityPlan": "Nueva Entidad del Plan",
+      "updateEntityPlan": "Actualizar Entidad del Plan",
+      "newLimit": "Nuevo Límite",
+      "updateLimit": "Actualizar Límite",
+      "newSubscription": "Nueva Suscripción",
+      "updateSubscription": "Actualizar Suscripción",
+      "newSubscriptionLimit": "Nuevo Límite de Suscripción",
+      "updateSubscriptionLimit": "Actualizar Límite de Suscripción",
+      "backToSubscriptions": "Atrás a Suscripciones",
+      "form": {
+        "category": "Categoría",
+        "plan": "Plan",
+        "limit": "Límites",
+        "subscription": "Suscripción",
+        "frequency": "Frecuencia",
+        "startDate": "Desde",
+        "endDate": "Hasta",
+        "quantity": "Cantidad",
+        "quantityUsed": "Cantidad Usada",
+        "entity": "Entidad",
+        "entityName": "Asignado a",
+        "attribute": "Atributo",
+        "attributeValue": "Valor del Atributo",
+        "module": "Módulo"
+      },
+      "options": {
+        "yes": "Sí",
+        "no": "No"
+      },
+      "documentation": {
+        "plans": "Con los planes puedes diseñar características y capacidades que ofrece un módulo a tus usuarios. Estos planes son diseñados para que el usuario pueda elegir de entre las opciones cual se acomoda mejor según el uso que necesite darle a tu aplicación.",
+        "limits": "Puedes limitar o restringir la características y capacidades de cada plan, según cada modulo. Estos limites pueden incluir desde cantidades hasta fechas limites",
+        "categories": "Es posible crear categories en tu Plan, con un nombre, descripción y organizarlas en modo de árbol (categorías padres y categorías hijas) con el fin de categorizar todos tus planes.",
+        "entityplans": "Es necesario que se especifique que módulos deben de estar limitados por los planes, de está manera la aplicación sabra como y cuando limitar las características que se definan en un plan.",
+        "subscriptions": "Cada plan que un usuario adquiera, se llamará subscripción, de está manera tu aplicación es capaz de identificar que características y capacidades le estas permitiendo a tus usuarios."
+      },
+      "helpFieldTrial": "Para habilitar una versión de prueba, ingresa el numero de días para el plan"
+    }
+  }
 }
